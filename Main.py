1a411c7079226cb84770b522d6942407418d7af2
karthikmutta2603 <156900697+karthikmutta2603@users.noreply.github.com>
2024-01-28 17:44:29 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -1,31 +1,47 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-
+import base64
 st.set_page_config(layout="wide")
 
+def show_pdf(file_path):
+        with open(file_path,"rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="400" height="600" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
 st.write('<style>div.block-container{padding-top:0rem;}</style>', unsafe_allow_html=True)
 
 with st.sidebar:
     selected = option_menu(
         menu_title="KARTHIK",
-        options=["Home","Experience", "Education", "Skills", "Contact"],
-        icons=["house", "clock history","book half", "tools", "envelope"],
+        options=["Home","Experience", "Education", "Skills", "Resume","Contact"],
+        icons=["house", "clock history","book half", "tools", "paperclip","envelope"],
         menu_icon="none",
         default_index=0,
     )
 
 if selected == "Home":
-    st.markdown("<div style='text-align:center;margin-top:0px'><h1>ABOUT ME</h1></div>",unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;margin-top:20px'><h1>ABOUT ME</h1></div>",unsafe_allow_html=True)
     with st.container():
         matter, photo = st.columns(2)
-        matter.write("<div style='font-size: 18px;'>Hi, I'm MUTTA KARTHIK PATTABHI RAMA RAO. I'm currently pursuing B.Tech 4th year in the department of Computer Science and Engineering at Sri Vasavi Engineering College, pedatadepalli affiliated to JNTUK</div>", unsafe_allow_html=True)
-        matter.write("<div style='font-size: 18px;'>▶ In addition, I like to 🏓 play table tennis,🎮 play video games, 🎨 drawing,🎧 listening to music and... 🍽️ enjoy eating good food in my free time!</div>", unsafe_allow_html=True)
-        matter.markdown("""
-             -  [linkedIN](https://www.linkedin.com/in/karthik-mutta-830413253/)
-            """)
+        with matter:
+            st.markdown("<div style='text-align:center;margin-top:80px'></div>",unsafe_allow_html=True)
+            st.write("<div style='font-size: 18px;'>Hi, I'm MUTTA KARTHIK PATTABHI RAMA RAO. I'm currently pursuing B.Tech 4th year in the department of Computer Science and Engineering at Sri Vasavi Engineering College, pedatadepalli affiliated to JNTUK</div>", unsafe_allow_html=True)
+            st.text("")
+            st.write("<div style='font-size: 18px;'>▶ In addition I like to play table tennis 🏓 ,  play video games 🎮 ,  drawing 🎨 , listening to music 🎧 and...  enjoy eating good food 🍽️ in my free time!</div>", unsafe_allow_html=True)
+            st.text("")
+            st.markdown("<div><h4>Social Media</h4></div>",unsafe_allow_html=True)
+            st.markdown("""
+                -  [linkedIN](https://www.linkedin.com/in/karthik-mutta-830413253/)
+                -  [Github](https://www.linkedin.com/in/karthik-mutta-830413253/)
+                -  [Instagram](https://www.linkedin.com/in/karthik-mutta-830413253/)
+                """)
+            st.markdown("<div><h4>Resume</h4></div>",unsafe_allow_html=True)
+            st.markdown("""
+                -  [Resume](https://drive.google.com/file/d/1jDk641XVjRTc5wtMz7C7wCg75nVJKJNQ/view?usp=drive_link)
+                """)
         photo.image("profile-pic.png")
 elif selected == "Experience":
-    st.markdown("<div style='text-align:center;margin-top:0px'><h1>EXPERIENCE</h1></div>",unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>EXPERIENCE</h1></div>",unsafe_allow_html=True)
     with st.container():
         image_column, text_column = st.columns((1,5))
         with image_column:
@@ -52,7 +68,7 @@ elif selected == "Experience":
             """)
 
 elif selected == "Skills":
-    st.markdown("<div style='text-align:center;margin-top:0px'><h1>SKILLS</h1></div>",unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>SKILLS</h1></div>",unsafe_allow_html=True)
     
     with st.container():
         left,middle,right = st.columns(3)
@@ -120,7 +136,7 @@ elif selected == "Skills":
                 st.write(c,"%")
                 st.text("")
 elif selected == "Contact":
-    st.markdown("<div style='text-align:center;margin-top:0px'><h1>FEEDBACK FORM</h1></div>",unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>FEEDBACK FORM</h1></div>",unsafe_allow_html=True)
     with st.form("Form1"):
         col1,col2 = st.columns(2)
         firstname = col1.text_input("First Name :")
@@ -136,7 +152,7 @@ elif selected == "Contact":
         else:
             st.success("Submited Successfully")
 elif selected == "Education":
-    st.markdown("<div style='text-align:center;margin-top:0px'><h1>EDUCATION</h1></div>",unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>EDUCATION</h1></div>",unsafe_allow_html=True)
     
     with st.container():
         col1, col2, col3 = st.columns(3)
@@ -155,3 +171,15 @@ elif selected == "Education":
             st.image("college.jpg", use_column_width=True)
             st.markdown("<div align='center'><b>Sri Vasavi Engineering College</b>  <br> <b> Bachelor of Technology</b> <br> <b>Computer Science and Engineering</b> <br> <b>Percentage - 79.34%</b></div>", unsafe_allow_html=True)
             st.text("")
+elif selected == "Resume":   
+    resume_url = "https://drive.google.com/file/d/1jDk641XVjRTc5wtMz7C7wCg75nVJKJNQ/view?usp=drive_link"
+    st.markdown("<div style='text-align:center;margin-top:10px'><h1>RESUME</h1></div>",unsafe_allow_html=True)
+    
+    show_pdf("MUTTA-KARTHIK-PATTABHI-RAMA-RAO-RESUME.pdf")
+    with open("MUTTA-KARTHIK-PATTABHI-RAMA-RAO-RESUME.pdf", "rb") as file:
+        btn = st.download_button(
+            label="Download Resume",
+            data=file,
+            file_name="MUTTA-KARTHIK-PATTABHI-RAMA-RAO-RESUME.pdf",
+            mime="application/pdf",type="primary"
+        )
